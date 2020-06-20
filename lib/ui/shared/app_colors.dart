6c6bfb332d0d66ed7f2b61f5f48caa94d85e7804
Adedeji Rotibi <dayjeerow7@gmail.com>
2020-06-20 21:59:53 +0100

    Complete Splash Screen

--- lib/ui/shared/app_colors.dart
+++ lib/ui/shared/app_colors.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 const Color lightGrey = Color.fromARGB(255, 61, 63, 69);
 const Color darkGrey = Color.fromARGB(255, 18, 18, 19);
-const Color primaryColor = Color(0xFF739AF0);
+const Color primaryColor = Color(0xFF5524FB);
 const Color backgroundColor = Color.fromARGB(255, 26, 27, 30);
 const Color inputFillColor = Color(0xFFEBF1FF);
 
--- lib/ui/views/intro/startup_screen.dart
+++ lib/ui/views/intro/startup_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:genotypematch/ui/shared/app_colors.dart';
+import 'package:genotypematch/ui/shared/ui_helpers.dart';
 import 'package:provider_architecture/provider_architecture.dart';
 
 import '../../../viewmodels/startup_view_model.dart';
@@ -33,22 +36,65 @@ class _StartUpScreenState extends State<StartUpScreen>
 
     return ViewModelProvider<StartUpViewModel>.withConsumer(
       viewModel: StartUpViewModel(),
-      onModelReady: (model) => model.handleStartUpLogic(),
+      // onModelReady: (model) => model.handleStartUpLogic(),
+      onModelReady: (model) => {},
       builder: (context, model, child) => AnimatedBuilder(
         animation: animationController,
         builder: (BuildContext ctx, Widget child) {
           return Scaffold(
+            backgroundColor: primaryColor,
             body: SafeArea(
-              child: Transform(
-                transform: Matrix4.translationValues(
-                  animation.value * width,
-                  0,
-                  0,
+              bottom: false,
+              child: Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/images/patternbg.png"),
+                    repeat: ImageRepeat.repeat,
+                    colorFilter: ColorFilter.mode(
+                      Colors.black.withOpacity(0.11),
+                      BlendMode.dstATop,
+                    ),
+                  ),
                 ),
-                child: Center(
-                  child: Image.asset(
-                    'assets/images/logo.png',
-                    width: 200,
+                child: Transform(
+                  transform: Matrix4.translationValues(
+                    animation.value * width,
+                    0,
+                    0,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        width: 103,
+                        height: 103,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(70),
+                            topRight: Radius.circular(20),
+                            bottomLeft: Radius.circular(70),
+                            bottomRight: Radius.circular(70),
+                          ),
+                          color: Colors.white,
+                        ),
+                        child: SvgPicture.asset(
+                          'assets/images/blood-drop-blue.svg',
+                          width: 40,
+                          color: primaryColor,
+                        ),
+                      ),
+                      verticalSpace(15),
+                      Text(
+                        "Blood Match.",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ),
